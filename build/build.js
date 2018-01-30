919c76a71734fee9315e5aea5ddb2753204bf6ad
fangtang0101 <623003648@qq.com>
2018-01-30 22:33:49 +0800

    1.build.js 注释

--- build/build.js
+++ build/build.js
@@ -1,25 +1,25 @@
 'use strict'
-require('./check-versions')()
+require('./check-versions')()         // # 引入 check-version 并执行，主要是版本控制的检查  文件 module.exports = function() (导出的就算一个func)
 
-process.env.NODE_ENV = 'production'
+process.env.NODE_ENV = 'production'   // # 指定全局变量 env.NODE_ENV 值为 production
 
-const ora = require('ora')
-const rm = require('rimraf')
+const ora = require('ora')            // # 一个很好看的 loading 插件
+const rm = require('rimraf')          // #  node环境下rm -rf的命令库  用于打包前删除 旧的文件
 const path = require('path')
 const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
 
-const spinner = ora('building for production...')
-spinner.start()
+const spinner = ora('building for production...')     // # 显示 loading 加文字提示
+spinner.start()                                       // # 显示菊花，开始转 菊花
 
-rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
-  if (err) throw err
-  webpack(webpackConfig, (err, stats) => {
+rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {  // #删除文件 文件路径(拼接而成) 
+  if (err) throw err                                                              // #如果删除 文件失败 或则报错，那么脏产诶 throw 出错误
+  webpack(webpackConfig, (err, stats) => {                                        // # webpack 开始打包 webpackConfig(文件路径)
     spinner.stop()
     if (err) throw err
-    process.stdout.write(stats.toString({
+    process.stdout.write(stats.toString({    // # ???   http://wiki.jikexueyuan.com/project/nodejs/process.html
       colors: true,
       modules: false,
       children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
